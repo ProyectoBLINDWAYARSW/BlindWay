6d4479158f7b30da86fec04f32cd97a74b76687a
Hugo Alvarez <hugoferneya@hotmail.com>
2016-10-29 21:54:26 -0500

    ahora si configurado

---
+++ src/main/java/edu/eci/arsw/blindway/BlindWay.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blindway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Hugo Alvarez
+ */
+@SpringBootApplication
+public class BlindWay {
+    public static void main(String[] args) { 		
+        SpringApplication.run(BlindWay.class, args); 
+    }
+}
