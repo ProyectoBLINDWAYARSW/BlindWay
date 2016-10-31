00a79e3d19473c765ac4567c38591871669e897b
Leonardo Herrera <leonardo.ft3@gmail.com>
2016-10-31 12:37:47 -0500

    Adelanto api sala

--- src/main/java/edu/eci/arsw/blindway/entities/Sala.java
+++ src/main/java/edu/eci/arsw/blindway/entities/Sala.java
@@ -26,13 +26,69 @@ public class Sala {
         
         public boolean ingresarSala(Usuario jugador){
             boolean verificacion=false;
-            if(jugador2==null){
-                if(!expulsados.contains(jugador)){
-                    jugador2=jugador;
+            if(getJugador2()==null){
+                if(!expulsados.contains(jugador) && !jugador1.getBloqueados().contains(jugador)){
+                    setJugador2(jugador);
                     verificacion=true;
                 }
             }
             return verificacion;
         }
+
+    /**
+     * @return the jugador1
+     */
+    public Usuario getJugador1() {
+        return jugador1;
+    }
+
+    /**
+     * @param jugador1 the jugador1 to set
+     */
+    public void setJugador1(Usuario jugador1) {
+        this.jugador1 = jugador1;
+    }
+
+    /**
+     * @return the jugador2
+     */
+    public Usuario getJugador2() {
+        return jugador2;
+    }
+
+    /**
+     * @param jugador2 the jugador2 to set
+     */
+    public void setJugador2(Usuario jugador2) {
+        this.jugador2 = jugador2;
+    }
+
+    /**
+     * @return the expulsados
+     */
+    public List<Usuario> getExpulsados() {
+        return expulsados;
+    }
+
+    /**
+     * @param expulsados the expulsados to set
+     */
+    public void setExpulsados(List<Usuario> expulsados) {
+        this.expulsados = expulsados;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 
 }
