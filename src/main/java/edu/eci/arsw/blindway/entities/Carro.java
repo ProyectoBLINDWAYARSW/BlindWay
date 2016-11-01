0ba320766dfb713835a71e7ffcd8eb18a81d7381
Hugo Alvarez <hugoferneya@hotmail.com>
2016-11-01 11:16:58 -0500

    carro implementado

--- src/main/java/edu/eci/arsw/blindway/entities/Carro.java
+++ src/main/java/edu/eci/arsw/blindway/entities/Carro.java
@@ -4,22 +4,27 @@ package edu.eci.arsw.blindway.entities;
 
 public class Carro {
 
-	private int x;
+    private int x;
+
+    private int y;
+
+    private String direccion;
+
+    public Carro(int x, int y, String direccion) {
+        this.x = x;
+        this.y = y;
+        this.direccion = direccion;
+    }
         
-        private int y;
 
-	private String direccion;
+    public void moverseX(int x) {
+        this.x+=x;
+    }
+    
+    public void moverseY(int y) {
+        this.y+=y;
+    }
 
-	public void moverse() {
-
-	}
-
-	public void dibujar() {
-
-	}
-
-	public void borrar() {
-
-	}
+	
 
 }
