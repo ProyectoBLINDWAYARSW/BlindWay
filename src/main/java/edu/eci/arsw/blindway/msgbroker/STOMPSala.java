ed2494d4eae4ee0995bba151cd055201806973d4
Leonardo Herrera <leonardo.ft3@gmail.com>
2016-11-03 18:35:43 -0500

    Avance broker sala - modificacion diagrama de clases

--- src/main/java/edu/eci/arsw/blindway/msgbroker/STOMPSala.java
+++ src/main/java/edu/eci/arsw/blindway/msgbroker/STOMPSala.java
@@ -5,10 +5,22 @@
  */
 package edu.eci.arsw.blindway.msgbroker;
 
+import edu.eci.arsw.blindway.entities.Invitacion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
 /**
  *
  * @author 2107262
  */
 public class STOMPSala {
-    
+    @Autowired
+    SimpMessagingTemplate msgt;
+    @MessageMapping("/invitacion")    
+    public void getInvitacion(Invitacion inv) throws Exception {    
+        synchronized(msgt){
+           inv.getDestino().añadirInvitacion(inv);
+        }   
+    }
 }
