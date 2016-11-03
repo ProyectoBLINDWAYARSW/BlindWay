3113b16a295db577d476b73907e22b9dc714c9da
Leonardo Herrera <leonardo.ft3@gmail.com>
2016-11-03 12:02:27 -0500

    Pruebas sala funcionales

--- src/test/java/edu/eci/arsw/blindway/test/SalasTest.java
+++ src/test/java/edu/eci/arsw/blindway/test/SalasTest.java
@@ -38,7 +38,7 @@ public class SalasTest {
         }catch(Exception ex){
             StubUsuario.getInstance().vaciarUsuarios();
             StubSala.getInstance().vaciarSalas();
-            fail("Error en la creacion de sala: " + ex.getMessage());
+            fail("Error en la creacion de sala (1): " + ex.getMessage());
         }
         
     }
@@ -59,8 +59,91 @@ public class SalasTest {
         }catch(Exception ex){
             StubUsuario.getInstance().vaciarUsuarios();
             StubSala.getInstance().vaciarSalas();
-            fail("Error en el ingreso a sala: " + ex.getMessage());
+            fail("Error en el ingreso a sala (2): " + ex.getMessage());
         }
         
+    }
+    //3. Permitir a un usuario unirse a una sala existente que no contenga un segundo jugador pero si contraseña
+    @Test
+    public void tercerTest(){
+        try{
+            StubUsuario.getInstance().registroUsuario("Leonardo Herrera",20,"Masculino","Saiga","contraseña123","leonardo.ft3@gmail.com");
+            StubUsuario.getInstance().registroUsuario("Hugo Alvares",18,"Masculino","masterHugo","contraseña154","hugo@gmail.com");
+            Usuario jugador1=StubUsuario.getInstance().cargarUsuarioPorNick("Saiga");
+            Usuario jugador2=StubUsuario.getInstance().cargarUsuarioPorNick("masterHugo");
+            int id = StubSala.getInstance().crearSala(jugador1,"contraseña");
+            StubSala.getInstance().ingresarSala(id,jugador2,"contraseña");
+            String sala = StubSala.getInstance().obtenerSala(id).toString();
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            assertEquals("Error al ingresar a una sala con contraseña sin segundo jugador(3)","Saiga, masterHugo, contraseña",sala);
+        }catch(Exception ex){
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            fail("Error en el ingreso a sala (3): " + ex.getMessage());
+        }    
+    }
+    //4. No permitir a un usuario unirse a una sala existente que contenga un segundo jugador
+    @Test
+    public void cuartoTest(){
+        try{
+            StubUsuario.getInstance().registroUsuario("Leonardo Herrera",20,"Masculino","Saiga","contraseña123","leonardo.ft3@gmail.com");
+            StubUsuario.getInstance().registroUsuario("Hugo Alvares",18,"Masculino","masterHugo","contraseña154","hugo@gmail.com");
+            StubUsuario.getInstance().registroUsuario("Stefany",21,"Femenino","Stefany288","contraseña182","stefany@gmail.com");
+            Usuario jugador1=StubUsuario.getInstance().cargarUsuarioPorNick("Saiga");
+            Usuario jugador2=StubUsuario.getInstance().cargarUsuarioPorNick("masterHugo");
+            Usuario jugador3=StubUsuario.getInstance().cargarUsuarioPorNick("Stefany288");
+            int id = StubSala.getInstance().crearSala(jugador1,"contraseña");
+            StubSala.getInstance().ingresarSala(id,jugador2,"contraseña");
+            StubSala.getInstance().ingresarSala(id,jugador3,"contraseña");
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            fail("Error, permitio ingrersar mas de 1 usuario a una sala (4)");
+        }catch(Exception ex){
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            assertEquals("Error al ingresar a una sala con segundo jugador(4)","No fue posible ingresar al jugador a la sala seleccionada.",ex.getMessage());
+            
+        }
+    }   
+    //5. No permitir a un usuario unirse a una sala si tiene una contraseña incorrecta
+    @Test
+    public void quintoTest(){
+        try{
+            StubUsuario.getInstance().registroUsuario("Leonardo Herrera",20,"Masculino","Saiga","contraseña123","leonardo.ft3@gmail.com");
+            StubUsuario.getInstance().registroUsuario("Hugo Alvares",18,"Masculino","masterHugo","contraseña154","hugo@gmail.com");
+            Usuario jugador1=StubUsuario.getInstance().cargarUsuarioPorNick("Saiga");
+            Usuario jugador2=StubUsuario.getInstance().cargarUsuarioPorNick("masterHugo");
+            int id = StubSala.getInstance().crearSala(jugador1,"contraseña");
+            StubSala.getInstance().ingresarSala(id,jugador2,"contra");
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            fail("Error, permitio ingrersar a un usuario con contraseña incorrecta (5)");
+        }catch(Exception ex){
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            assertEquals("Error al ingresar a una sala con contraseña incorrecta(5)","No fue posible ingresar al jugador a la sala seleccionada.",ex.getMessage());
+            
+        }
+    }
+    //6. No permitir a un usuario unirse a una sala inexistente
+    @Test
+    public void sextoTest(){
+        try{
+            StubUsuario.getInstance().registroUsuario("Leonardo Herrera",20,"Masculino","Saiga","contraseña123","leonardo.ft3@gmail.com");
+            StubUsuario.getInstance().registroUsuario("Hugo Alvares",18,"Masculino","masterHugo","contraseña154","hugo@gmail.com");
+            Usuario jugador1=StubUsuario.getInstance().cargarUsuarioPorNick("Saiga");
+            Usuario jugador2=StubUsuario.getInstance().cargarUsuarioPorNick("masterHugo");
+            int id = StubSala.getInstance().crearSala(jugador1,"contraseña");
+            StubSala.getInstance().ingresarSala(id+1,jugador2,"contra");
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            fail("Error, permitio ingrersar a un usuario a una slaa inexistente (6)");
+        }catch(Exception ex){
+            StubUsuario.getInstance().vaciarUsuarios();
+            StubSala.getInstance().vaciarSalas();
+            assertEquals("Error al dejar ingresar a una sala inexistente(6)","La sala no fue encontrada.",ex.getMessage());
+            
+        }
     }
 }
